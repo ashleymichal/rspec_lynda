f7fdf7810bc97a7db70a5c57223b7456c32cec8d
Ashley Michal <ashleymichal@gmail.com>
2015-11-09 17:27:31 -0600

    finished string extend specs

--- food_finder/spec/support/string_extend_spec.rb
+++ food_finder/spec/support/string_extend_spec.rb
@@ -2,31 +2,49 @@ describe 'String' do
 
   describe "#titleize" do
 
-    it "capitalizes each word in a string"
+    it "capitalizes each word in a string" do
+      expect("happy birthday!".titleize).to eq("Happy Birthday!")
+    end
     
-    it "works with single-word strings"
+    it "works with single-word strings" do
+      expect("hello".titleize).to eq("Hello")
+    end
     
-    it "capitalizes all uppercase strings"
+    it "capitalizes all uppercase strings" do
+      expect("I'M SHOUTING!".titleize).to eq("I'm Shouting!")
+    end
     
-    it "capitalizes mixed-case strings"
+    it "capitalizes mixed-case strings" do
+      expect("wAckY sTriNG".titleize).to eq("Wacky String")
+    end
     
   end
   
   describe '#blank?' do
 
-    it "returns true if string is empty"
+    it "returns true if string is empty" do
+      expect("".blank?).to be true
+    end
 
-    it "returns true if string contains only spaces"
+    it "returns true if string contains only spaces" do
+      expect("   ".blank?).to be true
+    end
 
-    it "returns true if string contains only tabs"
-    # Get a tab using a double-quoted string with \t
-    # example: "\t\t\t"
+    it "returns true if string contains only tabs" do
+      expect("\t\t\t".blank?).to be true
+    end
 
-    it "returns true if string contains only spaces and tabs"
+    it "returns true if string contains only spaces and tabs" do
+      expect(" \t ".blank?).to be true
+    end
     
-    it "returns false if string contains a letter"
+    it "returns false if string contains a letter" do
+      expect("t".blank?).to be false
+    end
 
-    it "returns false if string contains a number"
+    it "returns false if string contains a number" do
+      expect("2".blank?).to be false
+    end
     
   end
   
