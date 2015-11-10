2eacea902122eaf18e534367b55193360f84bd4f
Ashley Michal <ashleymichal@gmail.com>
2015-11-10 15:51:56 -0600

    complete restaurant specs

--- food_finder/spec/restaurant_spec.rb
+++ food_finder/spec/restaurant_spec.rb
@@ -3,23 +3,26 @@ require 'restaurant'
 describe Restaurant do
 
   let(:test_file) { 'spec/fixtures/restaurants_test.txt' }
-  let(:crescent) { Restaurant.new(:name => 'Crescent', :cuisine => 'paleo', :price => '321') }
+  let(:crescent) { Restaurant.new(
+                                  :name => 'Crescent', 
+                                  :cuisine => 'paleo', 
+                                  :price => '321') }
   
   describe 'attributes' do
   
     it "allow reading and writing for :name" do
-      crescent.name = 'Moon'
-      expect(crescent.name).to eq('Moon')
+      subject.name = 'Moon'
+      expect(subject.name).to eq('Moon')
     end
 
     it "allow reading and writing for :cuisine" do
-      crescent.cuisine = 'vegan'
-      expect(crescent.cuisine).to eq('vegan')
+      subject.cuisine = 'vegan'
+      expect(subject.cuisine).to eq('vegan')
     end
 
     it "allow reading and writing for :price" do
-      crescent.price = '25'
-      expect(crescent.price).to eq('25')
+      subject.price = '25'
+      expect(subject.price).to eq('25')
     end
     
   end
@@ -73,36 +76,32 @@ describe Restaurant do
   describe '#initialize' do
 
     context 'with no options' do
-      # subject would return the same thing
-      let(:no_options) { Restaurant.new }
 
       it 'sets a default of "" for :name' do
-        expect(no_options.name).to eq("")
+        expect(subject.name).to eq("")
       end
 
       it 'sets a default of "unknown" for :cuisine' do
-        expect(no_options.cuisine).to eq("unknown")
+        expect(subject.cuisine).to eq("unknown")
       end
 
       it 'does not set a default for :price' do
-        expect(no_options.price).to be_nil
+        expect(subject.price).to be_nil
       end
     end
     
     context 'with custom options' do
 
-      let(:crescent_and_vine) { Restaurant.new(:name => 'Crescent and Vine', :cuisine => 'French', :price => '14/glass') }
-      
       it 'allows setting the :name' do
-        expect(crescent_and_vine.name).to eq('Crescent and Vine')
+        expect(crescent.name).to eq('Crescent')
       end
 
       it 'allows setting the :cuisine' do
-        expect(crescent_and_vine.cuisine).to eq('French')
+        expect(crescent.cuisine).to eq('paleo')
       end
 
       it 'allows setting the :price' do
-        expect(crescent_and_vine.price).to eq('14/glass')
+        expect(crescent.price).to eq('321')
       end
 
     end
@@ -112,55 +111,63 @@ describe Restaurant do
   describe '#save' do
     
     it 'returns false if @@file is nil' do
+      expect(Restaurant.file).to be_nil
+
       expect(crescent.save).to be false
     end
     
     it 'returns false if not valid' do
-      crescent.price = nil
-      expect(crescent.save).to be false
+      Restaurant.load_file(test_file)
+      expect(Restaurant.file).not_to be_nil
+
+      expect(subject.save).to be false
     end
     
     it 'calls append on @@file if valid' do
       Restaurant.load_file(test_file)
-      expect(Restaurant.file).to receive(:append).once
+      expect(Restaurant.file).not_to be_nil
+
+      expect(Restaurant.file).to receive(:append).with(crescent)
       crescent.save
     end
     
   end
   
   describe '#valid?' do
-    subject { Restaurant.new }
     
     it 'returns false if name is nil' do
-      subject.name = nil
-      expect(subject.valid?).to be false
+      crescent.name = nil
+      expect(crescent.valid?).to be false
     end
 
     it 'returns false if name is blank' do
-      expect(subject.valid?).to be false
+      crescent.name = ""
+      expect(crescent.valid?).to be false
     end
 
     it 'returns false if cuisine is nil' do
-      subject.cuisine = nil
-      expect(subject.valid?).to be false
+      crescent.cuisine = nil
+      expect(crescent.valid?).to be false
     end
 
     it 'returns false if cuisine is blank' do
-      expect(subject.valid?).to be false
+      crescent.cuisine = ""
+      expect(crescent.valid?).to be false
     end
     
     it 'returns false if price is nil' do
-      expect(subject.valid?).to be false
+      crescent.price = nil
+      expect(crescent.valid?).to be false
     end
 
     it 'returns false if price is 0' do
-      subject.price = 0
-      expect(subject.valid?).to be false
+      crescent.price = 0
+      expect(crescent.valid?).to be false
     end
     
     it 'returns false if price is negative' do
-      subject.price = -1
-      expect(subject.valid?).to be false
+      crescent.price = -1
+      expect(crescent.valid?).to be false
     end
 
     it 'returns true if name, cuisine, price are present' do
